5980927f816066c0464024744681c24956a47157
Alexey Saveliev <Savelev6@ya.ru>
2023-06-06 12:07:54 +0500

    добавил один файл в другом формате

---
+++ uchi0506.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+lab = ['apple', 'xiaomi', 'samsung']
+sizes = [50, 30,20]
+
+fig, ax = plt.subplots()
+ax.pie(sizes, labels = lab)
+
+#Создаем
+
+#Выводим график
+plt.show()
+
